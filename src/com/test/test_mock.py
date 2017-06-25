9a96a4c68fb540d3f4122ffcc851bfaf0747c7b0
dbxiao <136187300@qq.com>
2017-06-25 21:49:00 +0800

    test

---
+++ src/com/test/test_mock.py
@@ -0,0 +1,43 @@
+#!/bin/env/python
+#-*- coding:UTF-8 -*-
+'''
+Created on 2017年6月25日
+
+@author: x
+'''
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.internet import reactor
+import time
+import logging
+import random
+from pip._vendor.distlib._backport.tarfile import filemode
+from pip._vendor.requests.api import request
+i=1
+log_filename="pymock.log"
+logging.basicConfig(filename=log_filename,filemode='w',level=logging.DEBUG)
+class FormPage(Resource):
+    def render_GET(self,request):
+        return '<html><body>this is mock</body></html>'
+    
+    def render_POST(self,request):
+        request.responseHeaders.addRawHeader("Content-Type", "text/html; charset=utf-8")
+        name = request.args['name'][0]
+        print 'name=',name
+        global i
+        #p=request.content.read()
+        #print p,':',len(p)
+        #logging.debug('post:[%d][%d][%s]',i,len(p),time.ctime())
+        print 'post:[',i,']'
+        i=i+1
+        result = '{"result":"ok","getString":"'+name+'","random":"'+ str(random.randint(1000,9999))+'"}'
+        return result
+        #return name
+logging.debug('begin:[%s]',time.ctime())
+root=Resource()
+root.putChild('', FormPage())
+factory = Site(root)
+reactor.listenTCP(7777,factory)
+reactor.run()
+
+        
