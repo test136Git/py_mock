6d3fba5c491aa5d948ae16eaf492f6737aca9157
dbxiao <136187300@qq.com>
2017-06-28 23:29:54 +0800

    3

--- src/com/test/test_mock.py
+++ src/com/test/test_mock.py
@@ -13,31 +13,30 @@ import logging
 import random
 from pip._vendor.distlib._backport.tarfile import filemode
 from pip._vendor.requests.api import request
+import uuid
+
 i=1
 log_filename="pymock.log"
 logging.basicConfig(filename=log_filename,filemode='w',level=logging.DEBUG)
 class FormPage(Resource):
     def render_GET(self,request):
-        return '<html><body>this is mock</body></html>'
+        return '<html><body>this is get method response . <br>uuid=%s</body></html>'%(uuid.uuid1())
     
     def render_POST(self,request):
-        request.responseHeaders.addRawHeader("Content-Type", "text/html; charset=utf-8")
+        request.responseHeaders.addRawHeader("Content-Type", "application/json; charset=utf-8")
         name = request.args['name'][0]
         print 'name=',name
         global i
-        #p=request.content.read()
-        #print p,':',len(p)
-        #logging.debug('post:[%d][%d][%s]',i,len(p),time.ctime())
         print 'post:[',i,']'
         i=i+1
-        result = '{"result":"ok","getString":"'+name+'","random":"'+ str(random.randint(1000,9999))+'"}'
+        result = '{"result":"ok","getString":"%s","uuid":"%s"}'%(name,uuid.uuid1())
         return result
         #return name
 logging.debug('begin:[%s]',time.ctime())
 root=Resource()
 root.putChild('', FormPage())
 factory = Site(root)
-reactor.listenTCP(7777,factory)
+reactor.listenTCP(80,factory)
 reactor.run()
 
         
